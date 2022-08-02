eb15408593c7714b78113a526b5e2b5ae70ab285
nnaucorp <niaz.nayeem@pmamediagroup.com>
2022-08-02 10:19:12 +0600

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="my-6 text-center text-6xl font-bold">Welcome to React</h1>
+    </div>
+  );
+}
+
+export default App;
