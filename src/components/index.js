2ff2322046f28e612df884f376c4d6ef4d9e6eb4
Sabbir Al Azim Shehab <56802016+sabbir421@users.noreply.github.com>
2022-08-06 01:46:17 +0600

    import solve

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,5 @@ export { default as Layout } from "./Layout";
 export { default as ListSelection } from "./ListSelection";
 export { default as Navbar } from "./Navber";
 export { default as SignUp } from "./SignUp";
-export { default as InviteMember } from "./Invite-member-project";
-export { default as Form } from "./Form/Form";
+export { default as InviteMember } from "./InviteMemberProject";
+export { default as Form } from "./Form";
