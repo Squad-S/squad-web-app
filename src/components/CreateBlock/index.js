026f038005af2cadf76bd13d473473cca987b298
jafoor <abujafor.cse11@gmail.com>
2022-08-06 07:06:29 +0600

    updated in omponent section

--- src/components/CreateBlock/index.js
+++ src/components/CreateBlock/index.js
@@ -56,7 +56,6 @@ export default function CreateBlock({
               </label>
               <input
                 {...inp.props}
-                {...(inp.isRequired ? "required" : "")}
                 className="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
               />
             </div>
