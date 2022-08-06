2f214078113030a6efdf65d36140d91021b1203f
jafoor <abujafor.cse11@gmail.com>
2022-08-06 13:03:28 +0600

    Updated spacing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Home, ProjectList, Organization} from "./pages";
+import { Home, ProjectList, Organization } from "./pages";
 
 function App() {
   return (
