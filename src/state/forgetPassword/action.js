9d8939aea593cb3ef56b0c1d6ecb245e0916bdd7
Al Amin PC <alaminrahman0106@gmaile.com>
2022-08-07 11:20:30 +0600

    forgot password(updated)

--- src/state/forgetPassword/action.js
+++ src/state/forgetPassword/action.js
@@ -3,10 +3,14 @@ import { publicPost } from "../../utilities/apiCaller";
 
 export const forgotPassword = createAsyncThunk(
   "forgotPassword/fetchPassword",
-  async (email) => {
-    const response = await publicPost("forgot-password", email);
-    if (response) {
-      return response.data;
+  async (email, { rejectWithValue }) => {
+    try {
+      const response = await publicPost("forgot-password", email);
+      if (response) {
+        return response.data;
+      }
+    } catch (err) {
+      return rejectWithValue(err);
     }
   }
 );
